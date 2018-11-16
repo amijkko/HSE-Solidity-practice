77236c0d14de0da7e7b54141624867adc9051f7f
Alex <amijkko@yandex.ru>
2018-11-16 18:36:40 +0300

    Update OwnableLottery.sol

---
+++ lesson3/OwnableLottery.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.19;
+
+import "./Ownable.sol";
+
+contract Vote is Ownable {
+    
+    mapping (address => bool) vote_map;
+    uint public counter = 0;
+    bool ended = false;
+    mapping(uint256 => address) public users;
+    uint256 countUsers = 0;
+    uint256 public balance ;
+    
+    
+    function distributePrize () public onlyOwner
+    {
+        uint256 winningNumber = uint256(block.blockhash(block.number - 1)) % countUsers;
+   
+
+        users[winningNumber].transfer(balance);    
+    
+    }
+    function increaseBalance() public payable {
+        balance +=  msg.value; 
+    }
+    
+    
+    function participate () {
+        require(vote_map[msg.sender] == false);
+        require(ended == false);
+        vote_map[msg.sender] = true;
+        users[countUsers] = msg.sender;
+        countUsers += 1;
+        
+    }
+}
